5579372f423b43de3d0398fb1bfda8a605c976a6
Jang Haemin <io@jhaemin.com>
2021-09-22 21:07:25 +0900

    Add decrement variant and implement update/view

--- src/lib.rs
+++ src/lib.rs
@@ -32,12 +32,14 @@ struct Model {
 // `Msg` describes the different events you can modify state with.
 enum Msg {
     Increment,
+    Decrement,
 }
 
 // `update` describes how to handle each `Msg`.
 fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
     match msg {
         Msg::Increment => model.counter += 1,
+        Msg::Decrement => model.counter -= 1,
     }
 }
 
@@ -50,7 +52,9 @@ fn view(model: &Model) -> Node<Msg> {
     div![
         "This is a counter: ",
         C!["counter"],
-        button![model.counter, ev(Ev::Click, |_| Msg::Increment),],
+        button!["-", ev(Ev::Click, |_| Msg::Decrement),],
+        div![model.counter,],
+        button!["+", ev(Ev::Click, |_| Msg::Increment),],
     ]
 }
 
